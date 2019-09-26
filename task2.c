e0915e0ce085c6ca51c742ca0dcb46a0bdaef290
kaleyveeravenkatasatyanukarajkumar <55250593+kaleyveeravenkatasatyanukarajkumar@users.noreply.github.com>
2019-09-26 20:23:18 +0530

    Add files via upload

---
+++ task2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+void main()
+
+{
+
+   float f,c;
+ 
+   printf(" enter the temperature in celsius: \n");
+  
+   scanf("%f",&c);
+ 
+   f=((c)*(1.8)+(32));
+ 
+   printf("%.2f degree in farenheit is %.2f\n ",c,f);
+
+
+
+
+
+
+
+}
